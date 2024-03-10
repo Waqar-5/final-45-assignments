f83afe8728d7e1b5c9217172cb5a22c7483e82d0
“[Waqar <[wa5134810@gamil.com]>
2024-03-10 08:06:22 +0500

    first commit

---
+++ assignment 2/main.ts
@@ -0,0 +1,15 @@
+//storing a person name in a variable
+
+let personname = "Roshan Ali";
+
+//printing a person name in a lowercase
+
+console.log(personname.toLowerCase());
+
+//printing a person name in a uppercase
+
+console.log(personname.toUpperCase());
+
+//printing a person name in titlecase
+
+console.log(personname.replace (/\b\w/g, (char) => char.toUpperCase()));
